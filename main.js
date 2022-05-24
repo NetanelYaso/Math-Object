29c461911b64dc70eabaac80dcd1ac21524fca0b
NetanelYaso <netanelyaso12@gmail.com>
2022-05-24 17:28:16 +0300

    math object -1

---
+++ main.js
@@ -0,0 +1,82 @@
+// function printRandomNumber(){
+// var rndnum= Math.floor(Math.random()*10);
+//   return rndnum;
+// }
+
+// function someFunc(x){
+//     var rndnum = Math.floor(Math.random()*x);
+//     return rndnum;
+// }
+
+// function printRndNumber(){
+//     var number = +prompt();
+//     var rndnum = Math.floor(Math.random()*10);
+//     if(rndnum==number){
+//         console.log('succeed');
+//     }
+//     else{
+//         console.log('fail');
+//     }   
+// }
+
+// function someFunc(){
+//     var x=[] ;
+//     for(var i=0; i<10; i++){
+//       x.push(Math.floor(Math.random()*100))  
+//     }  
+//     console.log(x)
+// }
+  
+// function findMax(){
+//     var numfromuser= +prompt()
+//    var rndnum = Math.floor(Math.random()*10)
+//     if(numfromuser>rndnum){
+//       return numfromuser
+//     }
+//     return rndnum
+// }
+
+//   function minFiveNumbers(){      
+//     var array =[]
+//       for(var i=0; i<5; i++){
+//         var numfromuser= +prompt()
+//         array += numfromuser
+//       }      
+//        console.log(Math.min(...array)) 
+//   }
+
+//   function printMinOrMax(){   
+//       var num= +prompt()
+//       var rndnum = Math.floor(Math.random()*100)
+//       if(rndnum%2==0){
+//           return Math.min(num)
+//       }
+//       return Math.max(num)
+//   }
+
+// function twoArrays(){
+//     var myarray = []
+//     var rndnum = Math.floor(Math.random()*364)
+//     for(var i=0; i<rndnum; i++){
+//     var randomnumber = Math.floor(Math.random()*873)
+//     myarray.push(randomnumber)
+//     }
+//     console.log(myarray)
+// }
+
+// function arrayAndindex(array1){
+//  var rndindex = Math.floor(Math.random()*array1.length);
+//     console.log(array1[rndindex]);
+// }
+
+// function changeChar(){
+//     var myName = prompt();
+//     var rndchar= Math.floor(Math.random()*myName.length);      
+//     myName[rndchar] = myName.charAt(rndchar).toUpperCase()+ myName.slice(1)
+//     console.log(myName)
+// }
+// changeChar()
+
+
+
+    
